b338d09fa43d5a09036fc451943032a4f543f5dc
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-12-15 20:15:41 +0100

    Features: Added simple menu, high/last score and game reset

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 let score = 0
+let scoreList = []
 let play = false
 let startPos = [1, 1]
 let snake = [startPos]
@@ -14,7 +15,13 @@ function generate() {
             playBoard.innerHTML += `<div class='play-field' id="${JSON.stringify([y, x])}"></div>`
         }
     }
-    createSnack()
+}
+function clearBoard() {
+    for(let x = 1; x <= 20; x++) {
+        for (let y = 1; y <= 20; y++) {
+            color(getPartDiv([y, x]), "black")
+        }
+    }
 }
 function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s*100))
@@ -24,6 +31,14 @@ function getPartDiv(postion) {
 }
 function color(partDiv, color) {
     partDiv.style.backgroundColor = color
+}
+function showHideMenu(){
+    const menu = document.getElementById("menu")
+    if (menu.style.display === "none"){
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
 }
 function disableDirections(position) {
     disabledDirection = []
@@ -43,10 +58,33 @@ function disableDirections(position) {
 
 function createSnack() {
     snacks = []
-    const snackX = Math.floor(Math.random() * 20) + 1
-    const snackY = Math.floor(Math.random() * 20) + 1
-    color(getPartDiv([snackY, snackX]), "green")
-    snacks.push(snackY, snackX)
+    while (true) {
+        const snackX = Math.floor(Math.random() * 20) + 1
+        const snackY = Math.floor(Math.random() * 20) + 1
+        if (!snake.includes([snackY, snackX])) {
+            color(getPartDiv([snackY, snackX]), "green")
+            snacks.push(snackY, snackX)
+            break
+        } else {
+            console.log("Snack in snake!!")
+        }
+    }
+
+
+}
+function resetGame() {
+    scoreList.push(score)
+    score = 0
+    play = false
+    startPos = [1, 1]
+    snake = [startPos]
+    snacks = []
+    direction = "right"
+    disabledDirection = ["left"]
+    const highestScore = document.getElementById("highest-score")
+    highestScore.innerHTML = `Your highest score is: ${Math.max(...scoreList)}`
+    const lastScore = document.getElementById("last-score")
+    lastScore.innerHTML = `Your last score is: ${scoreList[scoreList.length -1]}`
 }
 function move(direction) {
     let firstPart = snake[snake.length -1]
@@ -62,6 +100,7 @@ function move(direction) {
     const toRemove = snake.shift()
     color(getPartDiv(toRemove), "black")
     if (snake[snake.length -1][0] === snacks[0] && snake[snake.length -1][1] === snacks[1]) {
+        score += 1
         snake.push(snake[snake.length -1])
         createSnack()
     }
@@ -69,8 +108,12 @@ function move(direction) {
 document.addEventListener('keydown', async (event) => {
     let key = event.key
     if (key === " ") {
+        showHideMenu()
         play = !play
         while (play) {
+            if(snacks.length === 0) {
+                createSnack()
+            }
             document.addEventListener("keydown", async (event) => {
                 let key = event.key
                 if (key === "a" && !disabledDirection.includes("left")) {
@@ -86,10 +129,19 @@ document.addEventListener('keydown', async (event) => {
             move(direction)
             snake.map(partOfSnake => {
                 const partDiv = getPartDiv(partOfSnake)
-                color(partDiv, "red")
-                disableDirections(partOfSnake)
+                if (partDiv !== null) {
+                    color(partDiv, "red")
+                    disableDirections(partOfSnake)
+                } else {
+                    play = false
+                }
             })
             await sleep(1)
+            if (!play) {
+                showHideMenu()
+                resetGame()
+                clearBoard()
+            }
         }
     }
 })
